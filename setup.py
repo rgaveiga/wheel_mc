a84feca580b0300de936da126df08224f98cf95b
Roberto Gomes, PhD <roberto.veiga@ufabc.edu.br>
2024-04-11 10:00:13 -0300

    Creating the first files of the project

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup
+
+setup(name='Wheel-MC',
+      version='0.5',
+      description="Simulations of the Wheel strategy.",
+      requires=['scipy','numpy','matplotlib'],
+      author='Roberto Gomes de Aguiar Veiga',
+      packages=['wheel_mc'])
+
+
---
+++ wheel_mc/__init__.py
@@ -0,0 +1,3 @@
+from .wheel_mc import Simulation
+
+__version__="0.5"
